058614b6c53649ae7c54b2b07e59613c02516ffc
IFEOR <nazumy@bk.ru>
2020-03-03 10:33:51 +0600

    [+] New activity Added new empty activity with FrameLayout markup and transition to it from the first activity

--- app/src/main/java/com/ifeor/gitandroidschool/MainActivity.kt
+++ app/src/main/java/com/ifeor/gitandroidschool/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.ifeor.gitandroidschool
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_first.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+
+        activity_first_button_to_second_screen.setOnClickListener {
+            val intent = Intent(this, FragmentsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
